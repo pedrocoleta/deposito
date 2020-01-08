e300e3736fefe9f54fa45e54a016306a9fc64e59
pedro_coleta <pedro@hotmail.com>
2020-01-08 13:48:29 +0100

    2 - Cambio borrado variable saldoActual del Main

--- cuentas/Main.java
+++ cuentas/Main.java
@@ -3,7 +3,6 @@ public class Main {
 
     public static void main(String[] args) {
         CCuenta cuenta1;
-        double saldoActual;
 
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         operativa_cuenta(cuenta1, 0);

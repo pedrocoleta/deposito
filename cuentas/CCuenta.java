64c515d3810238029804a1a1b0b314bcdc9b850f
pedro_coleta <pedro@hotmail.com>
2020-01-08 17:42:19 +0100

    3 - Cambios de código y generación de Javadoc

--- cuentas/CCuenta.java
+++ cuentas/CCuenta.java
@@ -1,73 +1,167 @@
 package cuentas;
+
+/**
+ * Esta clase simula el comportamiento de una cuenta bancaría con sus atributos
+ * de nombre, CCC, saldo y tipo de interes y nos va a permitir crear cuentas
+ * bancarias y hacer operaciones de consulta de saldo, ingreso y retirada de
+ * dinero de la misma
+ * 
+ * @author pedrof
+ * @version 1.0
+ * @since 08/01/2020
+ * 
+ */
 public class CCuenta {
 
+	private String nombre;
+	private String cuenta;
+	private double saldo;
+	private double tipoInteres;
 
-    private String nombre;
-    private String cuenta;
-    private double saldo;
-    private double tipoInteres;
+	/**
+	 * Constructor por defecto de la clase CCuenta. Inicializará nombre y cuenta a
+	 * null y saldo y tipo de interes a 0.0
+	 */
+	public CCuenta() {
+	}
 
-    public CCuenta()
-    {
-    }
+	/**
+	 * Constructor parametrizado de la clase CCuenta. Inicializará nombre, cuenta y
+	 * saldo al valor pasado por parámetro al crear una instancia de la clase y
+	 * tipoInteres a 0.0 ya que no se asigna nada al mismo en la inicialización a
+	 * pesar de pedirla como parámetro
+	 * 
+	 * @param nom - Nombre de la cuenta a crear
+	 * @param cue - Número de la cuenta a crear
+	 * @param sal - Saldo inicial de la cuenta a crear
+	 * @param tipo - Tipo de interes aplicado a la cuenta a crear 
+	 */
+	public CCuenta(String nom, String cue, double sal, double tipo) {
+		setNombre(nom);
+		setCuenta(cue);
+		setSaldo(sal);
+	}
 
-    public CCuenta(String nom, String cue, double sal, double tipo)
-    {
-        setNombre(nom);
-        setCuenta(cue);
-        setSaldo(sal);
-    }
-
-    private String getNombre() {
+	/**
+	 * Devuelve el nombre de la cuenta bancaria consultada
+	 * 
+	 * @return nombre de la cuenta bancaria que llama al método
+	 */
+	public String getNombre() {
 		return nombre;
 	}
 
-	private void setNombre(String nombre) {
+	/**
+	 * Asigna el nonbre pasado por parámetro a la cuenta bancaria que ejecuta el
+	 * método
+	 * 
+	 * @param nombre Nombre a asignar a la cuenta que llama al método  
+	 */
+	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
-	private String getCuenta() {
+	/**
+	 * Devuelve el número de la cuenta bancaria consultada (CCC)
+	 * 
+	 * @return número de la cuenta bancaria que llama al método
+	 */
+	public String getCuenta() {
 		return cuenta;
 	}
 
-	private void setCuenta(String cuenta) {
+	/**
+	 * Asigna el número de cuenta pasado por parámetro a la cuenta bancaria que
+	 * ejecuta el método
+	 * 
+	 * @param cuenta Numero de cuenta a asignar a la cuenta que llama al método 
+	 */
+	public void setCuenta(String cuenta) {
 		this.cuenta = cuenta;
 	}
 
-	private double getSaldo() {
+	/**
+	 * Devuelve el saldo de la cuenta bancaria consultada
+	 * 
+	 * @return saldo de la cuenta bancaria que llama al método
+	 */
+	public double getSaldo() {
 		return saldo;
 	}
 
-	private void setSaldo(double saldo) {
+	/**
+	 * Asigna el saldo pasado por parámetro a la cuenta bancaria que ejecuta el
+	 * método
+	 * 
+	 * @param saldo Saldo a asignar a la cuenta que llama al método 
+	 */
+	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
 
-	private double getTipoInteres() {
+	/**
+	 * Devuelve el tipo de interés aplicado a la cuenta bancaria consultada
+	 * 
+	 * @return tipo de interés aplicado a la cuenta bancaria que llama al método
+	 */
+	public double getTipoInteres() {
 		return tipoInteres;
 	}
 
-	private void setTipoInteres(double tipoInteres) {
+	/**
+	 * Asigna el tipo de interés pasado por parámetro a la cuenta bancaria que
+	 * ejecuta el método
+	 * 
+	 * @param tipoInteres Tipo de interés a asignar a la cuenta que llama al método 
+	 */
+	public void setTipoInteres(double tipoInteres) {
 		this.tipoInteres = tipoInteres;
 	}
 
-	public double estado()
-    {
-        return getSaldo();
-    }
+	/**
+	 * Devuelve el estado del saldo de la cuenta bancaria consultada
+	 * 
+	 * @return saldo de la cuenta bancaria que llama al método
+	 */
+	public double estado() {
+		return getSaldo();
+	}
 
-    public void ingresar(double cantidad) throws Exception
-    {
-        if (cantidad<0)
-            throw new Exception("No se puede ingresar una cantidad negativa");
-        setSaldo(getSaldo() + cantidad);
-    }
+	/**
+	 * Ingresa la cantidad pasada por parámetro en la cuenta bancaria que ejecuta el
+	 * método Si el saldo a ingresar es negativo, contempla dicho error y no ingresa
+	 * nada
+	 * 
+	 * @param cantidad Cantidad a ingresar en la cuenta que llama al método.
+	 * @throws Exception Si la cantidad a ingresar es negativa, no se ingresa nada y se resuelven con una excepción
+	 * @see <a href =
+	 *      https://docs.oracle.com/javase/7/docs/api/java/lang/Exception.html> Clase
+	 *      Exception del API de java </a>
+	 */
+	public void ingresar(double cantidad) throws Exception {
+		if (cantidad < 0)
+			throw new Exception("No se puede ingresar una cantidad negativa");
+		setSaldo(getSaldo() + cantidad);
+	}
 
-    public void retirar(double cantidad) throws Exception
-    {
-        if (cantidad <= 0)
-            throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado()< cantidad)
-            throw new Exception ("No se hay suficiente saldo");
-        setSaldo(getSaldo() - cantidad);
-    }
+	/**
+	 * Retira la cantidad pasada por parámetro de la cuenta bancaria que ejecuta el
+	 * método Si el saldo de la cuenta bancaria es menor o igual a 0 o si se intenta
+	 * retirar una cantidad de dinero superior al saldo de la cuenta en cuestión, no
+	 * hará nada el método Como salida normal, se obtendrá una reducción del saldo
+	 * de la cuenta en la cantidad retirada
+	 * 
+	 * @param cantidad Cantidad a retirar en la cuenta que llama al método.
+	 * @throws Exception Si la cantidad a retirar es negativa o mayor al saldo actual de la cuenta, no se retira nada y se resuelven con una excepción
+	 * @see <a href =
+	 *      https://docs.oracle.com/javase/7/docs/api/java/lang/Exception.html> Clase
+	 *      Exception del API de java </a>
+	 */
+	public void retirar(double cantidad) throws Exception {
+		if (cantidad <= 0)
+			throw new Exception("No se puede retirar una cantidad negativa");
+		if (estado() < cantidad)
+			throw new Exception("No se hay suficiente saldo");
+		setSaldo(getSaldo() - cantidad);
+	}
 }
--- cuentas/Main.java
+++ cuentas/Main.java
@@ -3,15 +3,22 @@ public class Main {
 
     public static void main(String[] args) {
         CCuenta cuenta1;
+        CCuenta cuenta2;
 
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         operativa_cuenta(cuenta1, 0);
+        cuenta2 = new CCuenta();
+        operativa_cuenta(cuenta2, 0);
     }
 
 	private static void operativa_cuenta(CCuenta cuenta1, float cantidad) {
 		double saldoActual;
 		saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es "+ saldoActual );
+        System.out.println("El saldo actual es "+ cuenta1.getNombre());
+        System.out.println("El saldo actual es "+ cuenta1.getCuenta());
+        System.out.println("El saldo actual es "+ cuenta1.getSaldo());
+        System.out.println("El saldo actual es "+ cuenta1.getTipoInteres());
 
         try {
             cuenta1.retirar(2300);
